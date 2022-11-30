a3cddeb195eb1a78d4003cf4a1ebfcffe3f6be71
Your Name <4283301@myuwc.ac.za>
2022-11-30 21:28:07 +0200

    added js

--- main.js
+++ main.js
@@ -1,13 +1,53 @@
 const api ={
     key:"cfb7c1035e87a85901dfc28ab9bdf966",
+    baseurl:"http://api.openweathermap.org/data/2.5/forecast?q={city%20name}&appid=cfb7c1035e87a85901dfc28ab9bdf966 "
 }
 
-const searchbox=document.querySelector('.search-box');
+const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt){
     if (evt.keyCode == 13){
-        getResults(searchbox.value);
+       getResults(searchbox.value);
         console.log('searchbox.value')
     }
+}
+
+function getResults(query){
+    fetch('${api.base}weather?q=${query}&units=metric&APPID=${api.key}')
+    .then(weather => {
+        return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (query) {
+    let city = document.querySelector('.location .city');
+    city.innerText = '${weather.name}, ${weather.sys.country}';
+
+    let now = new date();
+    let date = document.querySelector('.location .date');
+    date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = '${Math.round(weather.main.temp)}<span>°C</span>';
+
+    let weather_el= document.querySelector('.current .weather');
+    weather_el.innerText=weather.weather[0].main;
+
+    let hilow =document.querySelector('.hi-low');
+    hilow.innerText='${Math.round(weather.main.temp_min)}°C / ${Math.round(weather.main.temp_max)}°C';
+}
+
+function dateBuilder(d) {
+    let months = ["January", "February" , "March" , "April" , "May" , "June" , "July" , "August","September",
+    "October" , "November" , "December"];
+
+    let days = ["Sunday","Monday","Tuesday","Wednesday" , "Thursday","Friday", "Saturday"];
+
+    let day = days [d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return '${day} ${date} ${month} ${year}';
 }
